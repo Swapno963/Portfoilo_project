7bd021687314d06ed35b2eaae49f3e570a1d6f9a
swapno963 <swapno963@gmail.com>
2023-12-08 01:02:32 +0600

    pagination done

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controlls');
-const secBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
-
-
-function pageTrnasition(){
-    // btn click active cls
-    // console.log(secBtn);
-    for(let i = 0; i< secBtn.length; i++){
-        secBtn[i].addEventListener('click',function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            console.log(currentBtn);
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
-            this.className  += ' active-btn'
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
         })
-    }
-}
-
-
-pageTrnasition()
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
